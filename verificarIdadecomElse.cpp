4d039848ecd31186fea99a0b849e6152cdcce11d
elihotchillipeper <150726982+elihotchillipeper@users.noreply.github.com>
2024-04-05 00:23:57 +0000

    exercicios (com else) 04/04

--- verificarIdadecomElse.cpp
+++ verificarIdadecomElse.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 
-int main(){
+int exercicio01(){
+
+//Verificar maioridade
 
     int idade;
 
@@ -15,4 +17,95 @@ int main(){
 
     return 0;
 
+}
+
+int exercicio02(){
+
+//Verificar paridade
+
+    int num;
+
+    printf("Digite um número: ");
+    scanf("%d", &num);
+
+    if (num % 2 ==0){
+        printf("O número é par.\n");
+    }else{
+        printf("O número é impar.\n");
+    }
+
+
+    return 0;
+}
+
+
+int exercicio03(){
+
+//Verificar positividade
+
+    int num;
+
+    printf("Digite um número: ");
+    scanf("%d", &num);
+
+    if(num > 0){
+        printf("O número é positivo.\n");
+    }else{
+        printf("O número é negativo.\n");
+    }
+
+
+    return 0;
+}
+
+
+int exercicio04(){
+
+//Verificar se é negativo
+
+    int num;
+
+    printf("Digite um número: ");
+    scanf("%d", &num);
+
+    if(num < 0){
+        printf("O número é negativo.\n");
+    }else{
+        printf("O número é positivo ou zero.\n");
+    }
+
+    return 0;
+}
+
+
+int exercicio05(){
+
+    //Verificar paridade
+
+    int num;
+
+    printf("Digite um número: ");
+    scanf("%d", &num);
+
+    if (num % 2 ==0){
+        printf("O número é par.\n");
+    }else{
+        printf("O número é impar.\n");
+    }
+
+
+    return 0;
+}
+
+
+
+
+
+
+int main(){
+    //exercicio01();
+    //exercicio02();
+    //exercicio03();
+    //exercicio04();
+    //exercicio05();
 }
