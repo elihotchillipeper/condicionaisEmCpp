775b53a47172557426033d27f70c0e33ce571d0f
elihotchillipeper <150726982+elihotchillipeper@users.noreply.github.com>
2024-04-08 19:18:38 -0300

    exercicio01.cpp

--- exercicio01.cpp
+++ exercicio01.cpp
@@ -29,12 +29,53 @@ int exercicio02(){
     return 0;
 
 }
+
+int exercicio03(){
+
+//Verificar se é negativo
+
+    int num;
+
+    printf("Digite um número: ");
+    scanf("%d", &num);
+
+    if(num < 0){
+        printf("O número é negativo.\n");
+    }else{
+        printf("O número é positivo ou zero.\n");
+    }
+
+    return 0;
+}
+
+
+int exercicio04(){
+
+    //Verificar paridade
+
+    int num;
+
+    printf("Digite um número: ");
+    scanf("%d", &num);
+
+    if (num % 2 ==0){
+        printf("O número é par.\n");
+    }else{
+        printf("O número é impar.\n");
+    }
+
+
+    return 0;
+}
+
     
 
 
 int main (){
     exercicio01();
     //exercicio02();
+    //exercicio03();
+    //exercicio04();
     return 0;
 }
 
