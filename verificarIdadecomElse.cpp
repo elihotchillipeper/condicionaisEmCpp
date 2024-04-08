3593548ed15496f3c7bcaaaf516c1120bfac436d
elihotchillipeper <150726982+elihotchillipeper@users.noreply.github.com>
2024-04-08 19:19:48 -0300

    Update verificarIdadecomElse.cpp

--- verificarIdadecomElse.cpp
+++ verificarIdadecomElse.cpp
@@ -59,46 +59,6 @@ int exercicio03(){
 }
 
 
-int exercicio04(){
-
-//Verificar se é negativo
-
-    int num;
-
-    printf("Digite um número: ");
-    scanf("%d", &num);
-
-    if(num < 0){
-        printf("O número é negativo.\n");
-    }else{
-        printf("O número é positivo ou zero.\n");
-    }
-
-    return 0;
-}
-
-
-int exercicio05(){
-
-    //Verificar paridade
-
-    int num;
-
-    printf("Digite um número: ");
-    scanf("%d", &num);
-
-    if (num % 2 ==0){
-        printf("O número é par.\n");
-    }else{
-        printf("O número é impar.\n");
-    }
-
-
-    return 0;
-}
-
-
-
 
 
 
@@ -106,6 +66,4 @@ int main(){
     //exercicio01();
     //exercicio02();
     //exercicio03();
-    //exercicio04();
-    exercicio05();
 }
